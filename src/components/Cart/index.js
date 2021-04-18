83ea0077d07166f16743e358741cac75bbdfcf31
Titux Metal <github@lgdweb.fr>
2021-04-18 18:35:26 +0200

    Updating index.js file in Cart; add the click event to the close button, using context

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { Modal } from '~/components/UI'
+import { ModalContext } from '~/context'
 
 import './CartItems.css'
 
 const Cart = () => {
   const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
+  const { toggleModalHandler } = useContext(ModalContext)
 
   return (
     <Modal>
@@ -19,7 +21,9 @@ const Cart = () => {
         <span>35.62</span>
       </div>
       <div className='actions'>
-        <button className='button--alt'>Close</button>
+        <button className='button--alt' onClick={toggleModalHandler}>
+          Close
+        </button>
         <button className='button'>Order</button>
       </div>
     </Modal>
