610c9a28b274cc67427abdc3b545310bf04d289a
Titux Metal <github@lgdweb.fr>
2021-04-18 14:29:51 +0200

    Adding a Cart component and its style

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,1 +1,29 @@
-export { default as CartIcon } from './CartIcon'
+import React from 'react'
+
+import { Modal } from '~/components/UI'
+
+import './CartItems.css'
+
+const Cart = () => {
+  const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
+
+  return (
+    <Modal>
+      <ul className='cart-items'>
+        {cartItems.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      <div className='total'>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className='actions'>
+        <button className='button--alt'>Close</button>
+        <button className='button'>Order</button>
+      </div>
+    </Modal>
+  )
+}
+
+export default Cart
