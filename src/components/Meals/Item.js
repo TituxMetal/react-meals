af5f3336bf722ad6f7112bd43f028536592a5a6f
Titux Metal <github@lgdweb.fr>
2021-04-19 21:46:24 +0200

    Updating Item.js file in meals; adding a function to add item to cart and pass it to the meal form

--- src/components/Meals/Item.js
+++ src/components/Meals/Item.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { CartContext } from '~/context'
 
 import './Item.css'
 import MealForm from './MealForm'
 
-const Item = ({ name, description, price }) => (
-  <li className='meal'>
-    <div>
-      <h3>{name}</h3>
-      <div className='description'>{description}</div>
-      <div className='price'>$ {price.toFixed(2)}</div>
-    </div>
-    <div>
-      <MealForm />
-    </div>
-  </li>
-)
+const Item = ({ id, name, description, price }) => {
+  const { addItemHandler } = useContext(CartContext)
+  const item = { id, name, price }
+
+  const addToCartHandler = amount => addItemHandler({ ...item, amount })
+
+  return (
+    <li className='meal'>
+      <div>
+        <h3>{name}</h3>
+        <div className='description'>{description}</div>
+        <div className='price'>$ {price.toFixed(2)}</div>
+      </div>
+      <div>
+        <MealForm onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  )
+}
 
 export default Item
