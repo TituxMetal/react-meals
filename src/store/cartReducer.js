dfbe6d70d6536df88af9a58d1c7f6a8e860c968a
Titux Metal <github@lgdweb.fr>
2021-04-22 10:08:08 +0200

    Updating cartReducer.js file in store; add and remove properly item to/from the cart

--- src/store/cartReducer.js
+++ src/store/cartReducer.js
@@ -1,10 +1,42 @@
 import { actions } from './actions'
 
+const findItemIndex = (items, id) => items.findIndex(item => item.id === id)
+
+const updateItem = (items, amount, id) =>
+  items.map(item =>
+    item.id === id ? { ...item, amount: item.amount + amount } : { ...item }
+  )
+
 const cartReducer = (state, { type, payload }) => {
   if (type === actions.ADD) {
     const totalAmount = state.totalAmount + payload.price * payload.amount
+    const existingItemIndex = findItemIndex(state.items, payload.id)
 
-    return { ...state, totalAmount, items: [...state.items, payload] }
+    if (existingItemIndex === -1) {
+      return { ...state, totalAmount, items: [...state.items, payload] }
+    }
+
+    const updatedItems = updateItem(state.items, payload.amount, payload.id)
+
+    return { ...state, totalAmount, items: updatedItems }
+  }
+
+  if (type === actions.REMOVE) {
+    const existingItemIndex = findItemIndex(state.items, payload)
+    const existingItem = state.items[existingItemIndex]
+    const totalAmount = state.totalAmount - existingItem.price
+
+    if (existingItem.amount === 1) {
+      return {
+        ...state,
+        totalAmount,
+        items: state.items.filter(item => item.id !== payload)
+      }
+    }
+
+    const updatedItems = updateItem(state.items, -1, payload)
+
+    return { ...state, totalAmount, items: updatedItems }
   }
 
   return state
