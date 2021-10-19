ca7ca245e3bfdc1a1358fb1866cab411efb588ae
Titux Metal (DEV) <github@lgdweb.fr>
2021-10-19 14:30:32 +0200

    Updating Card components; removes css files, replaced by tailwind twin.macro

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,24 +1,41 @@
 import React from 'react'
+import tw, { styled } from 'twin.macro'
 
-import './CartItem.css'
+const Actions = styled.div(() => [tw`flex flex-col`, tw`md:flex-row`])
+const Amount = styled.span(
+  tw`font-bold ring-2 ring-gray-700 ring-opacity-30 py-1 px-3 rounded-md text-gray-700`
+)
+const Button = styled.button(() => [
+  tw`font-bold text-lg text-red-900 text-center`,
+  tw`w-12 m-1 ml-4 cursor-pointer rounded-3xl`,
+  tw`ring-red-900 ring-2 ring-opacity-75 bg-transparent`,
+  tw`hover:(bg-red-900 text-gray-200 ring-0) active:(bg-red-900 text-gray-200 ring-0)`
+])
+const Info = styled.div(tw`w-40 flex justify-between items-center`)
+const Item = styled.li(
+  tw`flex justify-between items-center border-b-2 border-red-800 py-4 px-0 my-4 mx-0`
+)
+const Name = styled.h2(tw`mb-2 text-gray-800`)
+const Price = styled.span(tw`font-bold text-red-800`)
+const Wrapper = styled.div``
 
 const CartItem = ({ item, onRemove, onAdd }) => {
   const price = item.price.toFixed(2)
 
   return (
-    <li key={item.id} className='cart-item'>
-      <div>
-        <h2>{item.name}</h2>
-        <div className='info'>
-          <span className='price'>{price}</span>
-          <span className='amount'>x {item.amount}</span>
-        </div>
-      </div>
-      <div className='actions'>
-        <button onClick={() => onRemove(item.id)}>-</button>
-        <button onClick={() => onAdd({ ...item, amount: 1 })}>+</button>
-      </div>
-    </li>
+    <Item key={item.id}>
+      <Wrapper>
+        <Name>{item.name}</Name>
+        <Info>
+          <Price>{price}</Price>
+          <Amount>x {item.amount}</Amount>
+        </Info>
+      </Wrapper>
+      <Actions>
+        <Button onClick={() => onRemove(item.id)}>-</Button>
+        <Button onClick={() => onAdd({ ...item, amount: 1 })}>+</Button>
+      </Actions>
+    </Item>
   )
 }
 
--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,41 +1,42 @@
 import React, { useContext } from 'react'
+import tw, { styled } from 'twin.macro'
 
 import { Modal } from '~/components/UI'
 import { CartContext, ModalContext } from '~/context'
 
 import CartItem from './CartItem'
 
-import './Cart.css'
+const Actions = styled.div(tw`text-right`)
+const Button = styled.button(({ bordered }) => [
+  tw`font-bold text-lg text-gray-100 text-center`,
+  tw`py-1 px-4 ml-4 cursor-pointer rounded-3xl`,
+  tw`bg-red-700 border-2 border-transparent`,
+  tw`hover:(bg-red-900 text-gray-100) active:(bg-red-900 text-gray-200)`,
+  bordered && tw`text-red-900 bg-transparent border-2 border-red-900`
+])
+const Info = styled.div(tw`flex justify-between items-center font-bold text-2xl m-0 my-4`)
+const Items = styled.ul(tw`list-none m-0 p-0`)
 
 const Cart = () => {
-  const { items, totalAmount, addItemHandler, removeItemHandler } = useContext(
-    CartContext
-  )
+  const { items, totalAmount, addItemHandler, removeItemHandler } = useContext(CartContext)
   const { toggleModalHandler } = useContext(ModalContext)
   const hasItems = items.length > 0
 
   return (
     <Modal>
-      <ul className='cart-items'>
+      <Items>
         {items.map(item => (
-          <CartItem
-            key={item.id}
-            onAdd={addItemHandler}
-            onRemove={removeItemHandler}
-            item={item}
-          />
+          <CartItem key={item.id} onAdd={addItemHandler} onRemove={removeItemHandler} item={item} />
         ))}
-      </ul>
-      <div className='total'>
+      </Items>
+      <Info>
         <span>Total Amount</span>
         <span>${totalAmount.toFixed(2)}</span>
-      </div>
-      <div className='actions'>
-        <button className='button--alt' onClick={toggleModalHandler}>
-          Close
-        </button>
-        {hasItems && <button className='button'>Order</button>}
-      </div>
+      </Info>
+      <Actions>
+        <Button onClick={toggleModalHandler}>Close</Button>
+        {hasItems && <Button bordered>Order</Button>}
+      </Actions>
     </Modal>
   )
 }
