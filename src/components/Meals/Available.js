57d5d2866411f41e10eb36a3ec27ddebadb78b84
Titux Metal (DEV) <github@lgdweb.fr>
2021-10-25 19:43:10 +0200

    Updating Available.js file in components/Meals; uses the useHttp hook

--- src/components/Meals/Available.js
+++ src/components/Meals/Available.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { keyframes } from 'styled-components'
 import tw, { css, styled } from 'twin.macro'
 
 import { Card } from '~/components/UI'
+import { useHttp } from '~/hooks'
 
 import Item from './Item'
 
@@ -26,43 +27,30 @@ const Wrapper = styled.section(() => [
   `
 ])
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99
-  }
-]
-
 const Available = () => {
-  const mealsList = DUMMY_MEALS.map(({ id, name, description, price }) => (
+  const [items, setItems] = useState([])
+  const { isLoading, error, sendRequest } = useHttp()
+  const url = process.env.API_URI
+
+  const fetchMeals = useCallback(() => sendRequest({ url }, setItems), [])
+
+  useEffect(() => {
+    fetchMeals()
+  }, [fetchMeals])
+
+  const mealsList = items.map(({ id, name, description, price }) => (
     <Item key={id} name={name} description={description} price={price} id={id} />
   ))
 
   return (
     <Wrapper>
-      <Card>
-        <Items>{mealsList}</Items>
-      </Card>
+      {error && <p>{error}</p>}
+      {!isLoading && items.length > 0 && (
+        <Card>
+          <Items>{mealsList}</Items>
+        </Card>
+      )}
+      {isLoading && <p>Loading...</p>}
     </Wrapper>
   )
 }
